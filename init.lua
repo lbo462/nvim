462970acde812e370c467c76cf3be9fd77f299fc
lbo462 <leo.bonnaire@lbon.org>
2026-04-09 23:02:26 +0200

    chore: removed mouse

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.colorcolumn = '80'
+vim.opt.mouse = ''
 
 -- Keymaps
 vim.keymap.set('n', '<leader>r', '<cmd>!ruff format . --check && ruff check --no-fix .<CR>', { desc = 'Run ruff checks' })
